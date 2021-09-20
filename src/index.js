203f9beeace11585bf4e37a29d7c471791ee2a93
Gianni Zamora <giannizcool@gmail.com>
2021-09-20 15:03:09 -0700

    create React Crypto Market Newspage

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDom from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import store from './app/store';
+import 'antd/dist/antd.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+ReactDom.render(
+    // this is the react-router-dom wrapper to allow our links to work
+    <Router>
+        {/* This is the redux provider wrapper, every single component in our App will have access to store variable */}
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </Router>,
+     document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
